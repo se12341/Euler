6a9699d5d85271f37dfb26fce50ae3c98de25001
Naor Halevy <naorhalevy890@gmail.com>
2022-08-07 12:33:46 +0300

    Q3 - Done

---
+++ Question3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Numerics;
+namespace Primes
+{
+    class Question3
+    {
+       static void Main()
+       {
+        BigInteger menash = 600851475143 ;
+        for(BigInteger i = 50 ; i <= menash ; i++)
+            {
+                if(PrimeCheck(i))
+                    {
+                        if(menash % i == 0)
+                        {
+                        Console.Write(" this number is a prime: " + i + "\n");
+                        }
+                    }
+            }
+       }
+       public static bool PrimeCheck(BigInteger naor)
+       {
+        for(BigInteger j = 2 ; j < naor ; j++)
+        {
+            if(naor % j == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+       }
+    }
+}
