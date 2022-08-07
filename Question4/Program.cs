34dba7da955eb89482275e544a066f7bf1525115
Naor Halevy <naorhalevy890@gmail.com>
2022-08-07 19:48:05 +0300

    Q4 - Done

--- Question4/Program.cs
+++ Question4/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 using System.Numerics;
-namespace Primes
+namespace PalindromicNumbers
 {
     class Question3
     {
-       static void Main()
+       public static void Main()
        {
-        Console.Write("Hello World!");
+        int num = 0;
+        int max = 0;
+        string check;
+        string Rev;
+
+        for(int i = 100 ; i < 1000 ; i++)
+            {
+                for(int j = 100 ; j < 1000 ; j++)
+                {
+                    num = i*j;
+                    check = Convert.ToString(num);
+                    Rev = opposite(check);
+
+                    if(check == Rev)
+                    {
+                        if(num > max)
+                            max = num;
+                        Console.Write("The Largest palindrome Product is: " + max + "\n");
+                    }
+                }
+            }
        }
+        public static string opposite(string palindrome)
+        {
+            char[] cArr = palindrome.ToCharArray();
+            Array.Reverse(cArr);
+            return new string(cArr);
+        }
     }
 }
