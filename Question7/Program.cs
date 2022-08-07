785668a4b3015080482f6cc6d00cf201311d9523
Naor Halevy <naorhalevy890@gmail.com>
2022-08-07 22:52:50 +0300

    Q7 - Done

---
+++ Question7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Numerics;
+namespace BigPrime
+{
+    class Question7
+    {
+       static void Main()
+       {
+            BigInteger i = 2;
+            int sum = 1;
+            while(sum <= 10001)
+            {
+                if(PrimeCheck(i))
+                {
+                    Console.Write(i + "\n");
+                    sum++;
+                    //Console.Write(sum + "\n");
+                }
+                i++;
+            }
+
+       }
+       public static bool PrimeCheck(BigInteger naor)
+       {
+        for(BigInteger j = 2 ; j < naor ; j++)
+        {
+            if(naor % j == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+       }       
+    }
+}
