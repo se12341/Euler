95f6497e4bc1c07a34ccc3c607976462d674d04b
Naor Halevy <naorhalevy890@gmail.com>
2022-08-07 22:27:29 +0300

    Q6 - Done

---
+++ Question6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Numerics;
+namespace Primes
+{
+    class Question3
+    {
+       static void Main()
+       {
+        double sum = 0; 
+        double sqr = 0;
+        double total = 0;
+            for(int i = 1 ; i <= 100 ; i++)
+            {
+                sum = sum + Math.Pow(i , 2);
+                sqr = sqr + i;
+            }
+        sqr = Math.Pow(sqr, 2);
+        total = sqr - sum;
+        Console.Write(total);
+       }
+    }
+}
