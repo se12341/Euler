a64b6a4fb5e01afbdac384abd2806a454b4ccdc9
Naor Halevy <naorhalevy890@gmail.com>
2022-08-07 22:10:47 +0300

    Q5 - Done

--- Question5/Program.cs
+++ Question5/Program.cs
@@ -6,7 +6,26 @@ namespace Primes
     {
        static void Main()
        {
-        Console.Write("Hello World!");
+        int m = 100;
+        Boolean t;
+        while(m > 1)
+        {
+            t = NoRemainder(m);
+            if(t == true)
+                Console.Write(m);
+            m++;
+        }
+       }
+
+       public static Boolean NoRemainder(int number)
+       {
+            for(int i = 1 ; i < 20 ; i++)
+            {
+                if(number % i != 0 )
+                    return false;
+
+            }
+        return true;
        }
     }
 }
