249f2bbaf45bbaa92b6e1b13ecc33ed5317d0d23
Naor Halevy <naorhalevy890@gmail.com>
2022-08-07 12:33:40 +0300

    Q2 - Done

---
+++ Question2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Numerics;
+namespace EvenFibonacci 
+{
+    class Question2
+    {
+       static void Main()
+       {
+            int total = 0;
+            int a = 1;
+            int b = 1;
+            int ph = a + b;
+            int limiter = 4000000;
+
+            while(b < limiter)
+                {
+                    total = total + ph ; 
+                    a = b + ph ; 
+                    b = ph + a ;
+                    ph = a + b;
+                }
+            Console.Write(total);
+       }
+    }
+}
