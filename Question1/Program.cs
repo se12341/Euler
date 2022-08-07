8734ddb9e00be7dc8b185350074155d15436ee3e
Naor Halevy <naorhalevy890@gmail.com>
2022-08-07 12:33:34 +0300

    Q1 - Done

---
+++ Question1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Numerics;
+namespace ThreesAndFives
+{
+    class Question1
+    {
+       static void Main()
+       {
+            int threes = 0 ;
+            int fives = 0 ;
+            int sum = 0; 
+            for(int i = 0 ; i < 1000 ; i++)
+            {
+                if(i % 3 == 0 && i % 5 != 0)
+                    threes = threes + i ;
+                if(i % 5 == 0 )
+                   fives = fives + i ;
+
+                sum = threes + fives ;
+            }
+        Console.Write(sum);
+       }
+    }   
+}
